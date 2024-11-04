c5674521c9a1b714c1834c4988dacfcd74e35a5d
thsamajiki <ant1102@naver.com>
2024-11-04 15:27:47 +0900

    불필요한 enum class 제거

--- app/src/main/java/com/hero/z_market/domain/enum/ClassifiedLevel.kt
+++
@@ -1,5 +0,0 @@
-package com.hero.z_market.domain.enum
-
-enum class ClassifiedLevel(val level: String) {
-    BIG_CLASSIFIED("대분류"), MIDDLE_CLASSIFIED("중분류")
-}
--- app/src/main/java/com/hero/z_market/domain/enum/YesNo.kt
+++
@@ -1,5 +0,0 @@
-package com.hero.z_market.domain.enum
-
-enum class YesNo(val value: String) {
-    Y("Y"), N("N")
-}
