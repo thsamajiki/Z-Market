da8a4b349fbc11860945778cc7455bf6323de8dc
thsamajiki <ant1102@naver.com>
2024-11-04 15:34:29 +0900

    MainActivity의 위치 이동

--- app/src/main/java/com/hero/z_market/ui/MainActivity.kt
+++ app/src/main/java/com/hero/z_market/ui/MainActivity.kt
@@ -1,41 +1,36 @@
-package com.hero.z_market
+package com.hero.z_market.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.hero.z_market.ui.screen.parentCategory.MainScreen
 import com.hero.z_market.ui.theme.ZMarketTheme
+import com.hero.z_market.ui.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ZMarketTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val vm: MainViewModel by viewModels()
+                    MainScreen(vm)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text (text = "Hello $name!")
-}
-
-@Preview
-@Composable
-fun PreviewGreeting() {
-    Greeting("Android")
+    companion object {
+        const val PARENT_CATEGORY = "parentCategory"
+    }
 }
