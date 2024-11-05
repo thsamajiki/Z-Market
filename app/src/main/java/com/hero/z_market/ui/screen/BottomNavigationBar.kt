757bf273d45e76ffea164b13c1c76e57145db9b0
thsamajiki <ant1102@naver.com>
2024-11-05 15:25:08 +0900

    Main 화면 하단에 BottomNavigationBar 배치

---
+++ app/src/main/java/com/hero/z_market/ui/screen/BottomNavigationBar.kt
@@ -0,0 +1,110 @@
+package com.hero.z_market.ui.screen
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.hero.z_market.R
+import com.hero.z_market.ui.theme.ZMarketTheme
+import com.hero.z_market.ui.theme.selectedMenu
+import com.hero.z_market.ui.theme.unselectedMenu
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(
+        BottomNavItem.Categories,
+        BottomNavItem.Favorites,
+        BottomNavItem.Home,
+        BottomNavItem.Cart,
+        BottomNavItem.MyInfo
+    )
+    BottomNavigation(
+        backgroundColor = White,
+        contentColor = Color(0xFF3F414E)
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = item.icon),
+                        contentDescription = item.title,
+                        modifier = Modifier
+                            .size(24.dp),
+                        tint = if (currentRoute == item.screenRoute) {
+                            selectedMenu
+                        } else {
+                            unselectedMenu
+                        }
+                    )
+                },
+                // label 선택 안했을 때도 보이는 메뉴 이름
+                label = {
+                    if (currentRoute == item.screenRoute) {
+                        Text(
+                            item.title,
+                            fontSize = 9.sp,
+                            color = selectedMenu
+                        )
+                    } else {
+                        Text(
+                            item.title,
+                            fontSize = 9.sp,
+                            color = unselectedMenu
+                        )
+                    }
+                },
+                selectedContentColor = selectedMenu,
+                unselectedContentColor = unselectedMenu,
+                selected = currentRoute == item.screenRoute,
+                alwaysShowLabel = currentRoute == item.screenRoute,
+                onClick = {
+                    navController.navigate(item.screenRoute) {
+                        navController.graph.startDestinationRoute?.let {
+                            popUpTo(it) { saveState = true }
+                        }
+
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+// 바텀 메뉴 아이템 설정
+sealed class BottomNavItem(
+    val title: String, val icon: Int, val screenRoute: String,
+) {
+    data object Categories : BottomNavItem("카테고리", R.drawable.icon_menu_categories, "CATEGORIES")
+    data object Favorites : BottomNavItem("즐겨찾기", R.drawable.icon_menu_favorites, "FAVORITES")
+    data object Home : BottomNavItem("홈", R.drawable.icon_menu_home, "HOME")
+    data object Cart : BottomNavItem("장바구니", R.drawable.icon_menu_cart, "CART")
+    data object MyInfo : BottomNavItem("나의 정보", R.drawable.icon_menu_my_info, "MY_INFO")
+}
+
+@Composable
+@Preview(showBackground = true)
+fun BottomNavigationBarPreview() {
+    val navController = rememberNavController() // 가짜 NavHostController 생성
+
+    ZMarketTheme {
+        BottomNavigationBar(navController)
+    }
+}
