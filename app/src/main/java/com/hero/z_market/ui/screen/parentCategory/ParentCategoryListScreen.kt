024a8b266785d6d82db86ca6300842f202a31aae
thsamajiki <ant1102@naver.com>
2024-11-05 15:29:32 +0900

    ParentCategoryList 생성

---
+++ app/src/main/java/com/hero/z_market/ui/screen/parentCategory/ParentCategoryListScreen.kt
@@ -0,0 +1,50 @@
+package com.hero.z_market.ui.screen.parentCategory
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import com.hero.z_market.domain.model.ParentCategoryModel
+import com.hero.z_market.ui.preview.parentCategory.ParentCategoryListPreviewParameterProvider
+import com.hero.z_market.ui.theme.ZMarketTheme
+
+@Composable
+fun ParentCategoryListScreen(
+    parentCategoryList: List<ParentCategoryModel>,
+    onClicked: (ParentCategoryModel) -> Unit,
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(4),
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(10.dp, 10.dp)
+    ) {
+        items(count = parentCategoryList.size) { index ->
+            val item = parentCategoryList[index]
+            ParentCategoryItem(
+                parentCategory = item,
+                modifier = Modifier.padding(4.dp),
+                onClicked,
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ParentCategoryListScreenPreview(
+    @PreviewParameter(ParentCategoryListPreviewParameterProvider::class)
+    parentCategoryList: List<ParentCategoryModel>
+) {
+    ZMarketTheme {
+        ParentCategoryListScreen(parentCategoryList, onClicked = {})
+    }
+}
