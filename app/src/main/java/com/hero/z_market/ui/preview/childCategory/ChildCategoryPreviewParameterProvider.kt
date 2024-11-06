ccae88e5b046f8de2c586b79a857ea9dbf66ecc2
thsamajiki <ant1102@naver.com>
2024-11-06 14:11:27 +0900

    ChildCategoryPreviewParameterProvider 생성

---
+++ app/src/main/java/com/hero/z_market/ui/preview/childCategory/ChildCategoryPreviewParameterProvider.kt
@@ -0,0 +1,46 @@
+package com.hero.z_market.ui.preview.childCategory
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.hero.z_market.domain.model.ChildCategoryModel
+
+class ChildCategoryPreviewParameterProvider : PreviewParameterProvider<ChildCategoryModel> {
+    override val values = sequenceOf(
+        ChildCategoryModel(
+            childCategorySeq = 0,
+            childCategoryName = "상품 전체",
+            parentCategorySeq = 62,
+            childCategoryCode = "DPDCM23091",
+            childCategoryLevel = "중분류",
+            isSelected = true,
+        ),
+        ChildCategoryModel(
+            childCategorySeq = 52,
+            childCategoryName = "채소과일",
+            parentCategorySeq = 62,
+            childCategoryCode = "DPDCM23091",
+            childCategoryLevel = "중분류",
+            isSelected = false,
+        ),ChildCategoryModel(
+            childCategorySeq = 53,
+            childCategoryName = "농축수산물",
+            parentCategorySeq = 63,
+            childCategoryCode = "DPDCM23091",
+            childCategoryLevel = "중분류",
+            isSelected = false,
+        ),ChildCategoryModel(
+            childCategorySeq = 54,
+            childCategoryName = "식기",
+            parentCategorySeq = 64,
+            childCategoryCode = "DPDCM23091",
+            childCategoryLevel = "중분류",
+            isSelected = false,
+        ),ChildCategoryModel(
+            childCategorySeq = 55,
+            childCategoryName = "유제품",
+            parentCategorySeq = 65,
+            childCategoryCode = "DPDCM23091",
+            childCategoryLevel = "중분류",
+            isSelected = false,
+        ),
+    )
+}
