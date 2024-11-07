7134f0639f5b3e02cded2c8a45a0a7f02c5e9be1
thsamajiki <ant1102@naver.com>
2024-11-07 20:52:24 +0900

    PaginationInfoScreen 생성

---
+++ app/src/main/java/com/hero/z_market/ui/screen/childCategory/PaginationInfoScreen.kt
@@ -0,0 +1,42 @@
+package com.hero.z_market.ui.screen.childCategory
+
+import android.graphics.Typeface
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hero.z_market.data.response.PaginationItem
+
+@Composable
+fun PaginationInfoScreen(
+    pagination: PaginationItem,
+) {
+    Row(
+        modifier = Modifier.padding(start = 20.dp, top = 30.dp),
+    ) {
+        Text(
+            text = pagination.totalElements.toString(),
+            fontSize = 13.sp,
+            fontFamily = FontFamily(Typeface.SANS_SERIF),
+            fontWeight = FontWeight.SemiBold,
+            style = MaterialTheme.typography.h2,
+            color = Black,
+        )
+        Text(
+            modifier = Modifier.padding(start = 0.5.dp),
+            text = "건의 검색결과",
+            fontSize = 13.sp,
+            fontFamily = FontFamily(Typeface.SANS_SERIF),
+            fontWeight = FontWeight.SemiBold,
+            style = MaterialTheme.typography.h2,
+            color = Black,
+        )
+    }
+}
