94111b5eccbe13e9b2a10cff701ec792c1914263
thsamajiki <ant1102@naver.com>
2024-11-07 22:14:15 +0900

    RepositoryImpl 위치 이동에 따라 import 수정

--- app/src/main/java/com/hero/z_market/di/RepositoryModule.kt
+++ app/src/main/java/com/hero/z_market/di/RepositoryModule.kt
@@ -1,11 +1,11 @@
 package com.hero.z_market.di
 
+import com.hero.z_market.data.repository.ChildCategoryRepositoryImpl
+import com.hero.z_market.data.repository.GoodsRepositoryImpl
+import com.hero.z_market.data.repository.ParentCategoryRepositoryImpl
 import com.hero.z_market.domain.repository.ChildCategoryRepository
-import com.hero.z_market.domain.repository.ChildCategoryRepositoryImpl
 import com.hero.z_market.domain.repository.GoodsRepository
-import com.hero.z_market.domain.repository.GoodsRepositoryImpl
 import com.hero.z_market.domain.repository.ParentCategoryRepository
-import com.hero.z_market.domain.repository.ParentCategoryRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -18,18 +18,18 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun bindParentCategoryRepository(
-        parentCategoryRepositoryImpl: ParentCategoryRepositoryImpl
+        parentCategoryRepositoryImpl: ParentCategoryRepositoryImpl,
     ): ParentCategoryRepository
 
     @Singleton
     @Binds
     abstract fun bindChildCategoryRepository(
-        childCategoryRepositoryImpl: ChildCategoryRepositoryImpl
+        childCategoryRepositoryImpl: ChildCategoryRepositoryImpl,
     ): ChildCategoryRepository
 
     @Singleton
     @Binds
     abstract fun bindGoodsRepository(
-        goodsRepositoryImpl: GoodsRepositoryImpl
+        goodsRepositoryImpl: GoodsRepositoryImpl,
     ): GoodsRepository
 }
