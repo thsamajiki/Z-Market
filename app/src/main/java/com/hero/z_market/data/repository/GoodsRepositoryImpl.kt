73bc998141e87becd025288d1fd35e6060a51d8a
thsamajiki <ant1102@naver.com>
2024-11-07 19:59:03 +0900

    GoodsRepositoryImpl 생성

---
+++ app/src/main/java/com/hero/z_market/data/repository/GoodsRepositoryImpl.kt
@@ -0,0 +1,60 @@
+package com.hero.z_market.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.hero.z_market.data.remote.GoodsDataSource
+import com.hero.z_market.data.remote.GoodsPagingSource
+import com.hero.z_market.data.response.PaginationItem
+import com.hero.z_market.data.response.toEntity
+import com.hero.z_market.domain.model.GoodsModel
+import com.hero.z_market.domain.repository.GoodsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GoodsRepositoryImpl @Inject constructor(
+    private val goodsDataSource: GoodsDataSource
+): GoodsRepository {
+    override suspend fun fetchGoods(
+        parentCategorySeq: Int,
+        childCategorySeq: Int,
+        page: Int,
+        size: Int,
+        query: String,
+    ): Flow<PagingData<GoodsModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20, // 한 번에 읽을 데이터 수
+                enablePlaceholders = false
+            ),
+            initialKey = GoodsPagingSource.INITIAL_KEY,
+            pagingSourceFactory = {
+                GoodsPagingSource(goodsDataSource, parentCategorySeq, childCategorySeq, query)
+            }
+        )
+            .flow
+            .map { pagingData ->
+                pagingData.map { appGoodsInfoDTO ->
+                    appGoodsInfoDTO.toEntity()
+                }
+            }.flowOn(Dispatchers.IO)
+    }
+
+    override suspend fun fetchPaginationInfo(
+        parentCategorySeq: Int,
+        childCategorySeq: Int,
+        page: Int,
+        size: Int,
+        query: String,
+    ): Flow<PaginationItem> {
+        return flow {
+            val response = goodsDataSource.fetchGoods(parentCategorySeq, childCategorySeq, page, size, query).pagination.toEntity()
+            emit(response)
+        }.flowOn(Dispatchers.IO)
+    }
+}
