30f8273d3a6f40eec3b9b3109daa3e869875ae09
thsamajiki <ant1102@naver.com>
2024-11-07 20:54:51 +0900

    GoodsSort로 클래스명 변경

--- app/src/main/java/com/hero/z_market/domain/enum/GoodsSort.kt
+++ app/src/main/java/com/hero/z_market/domain/enum/GoodsSort.kt
@@ -1,6 +1,6 @@
 package com.hero.z_market.domain.enum
 
-enum class SearchOrderOption(val order: String) {
+enum class GoodsSort(val value: String) {
     RECOMMENDED("추천순"),
     BOUGHT("판매량순"),
     ASCENDING("낮은가격순"),
