5054019a7548b8286084da06d34d78f23f32dc84
thsamajiki <ant1102@naver.com>
2024-11-14 01:06:15 +0900

    Compose 버전 업그레이드에 따라 Deprecated 아이콘 imageVector 수정

--- app/src/main/java/com/hero/z_market/ui/screen/childCategory/ChildCategoryGoodsListTopAppBar.kt
+++ app/src/main/java/com/hero/z_market/ui/screen/childCategory/ChildCategoryGoodsListTopAppBar.kt
@@ -2,7 +2,7 @@ package com.hero.z_market.ui.screen.childCategory
 
 import android.widget.Toast
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -45,7 +45,7 @@ fun ChildCategoryGoodsListTopAppBar(
         navigationIcon = {
             IconButton(onClick = onBackClick) {
                 Icon(
-                    imageVector = Icons.Default.ArrowBack,
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                     contentDescription = "Back to MainActivity"
                 )
             }
