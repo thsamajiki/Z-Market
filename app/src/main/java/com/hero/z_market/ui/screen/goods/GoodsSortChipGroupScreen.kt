cc32b34f96ca889641acda5de1a5be6b08177184
thsamajiki <ant1102@naver.com>
2024-11-14 00:43:55 +0900

    Compose 버전 업그레이드에 따라 프로퍼티명 수정

--- app/src/main/java/com/hero/z_market/ui/screen/goods/GoodsSortChipGroupScreen.kt
+++ app/src/main/java/com/hero/z_market/ui/screen/goods/GoodsSortChipGroupScreen.kt
@@ -3,7 +3,7 @@ package com.hero.z_market.ui.screen.goods
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -14,29 +14,29 @@ import com.hero.z_market.ui.viewmodel.ChildCategoryGoodsListViewModel
 fun GoodsSortChipGroupScreen(
     vm: ChildCategoryGoodsListViewModel
 ) {
-    var selectedChipIndex = remember { mutableStateOf(0) } // 선택된 Chip의 인덱스 (-1은 아무것도 선택되지 않음을 의미)
+    var selectedChipIndex = remember { mutableIntStateOf(0) } // 선택된 Chip의 인덱스 (-1은 아무것도 선택되지 않음을 의미)
 
     Row(
         modifier = Modifier.padding(start = 10.dp, top = 10.dp),
     ) {
         GoodsSortChip(
             vm,
-            isSelected = selectedChipIndex.value == 0
-        ) { selectedChipIndex.value = 0 }
+            isSelected = selectedChipIndex.intValue == 0
+        ) { selectedChipIndex.intValue = 0 }
         GoodsSortChip(
             vm,
             GoodsSort.BOUGHT,
-            isSelected = selectedChipIndex.value == 1
-        ) { selectedChipIndex.value = 1 }
+            isSelected = selectedChipIndex.intValue == 1
+        ) { selectedChipIndex.intValue = 1 }
         GoodsSortChip(
             vm,
             GoodsSort.ASCENDING,
-            isSelected = selectedChipIndex.value == 2
-        ) { selectedChipIndex.value = 2 }
+            isSelected = selectedChipIndex.intValue == 2
+        ) { selectedChipIndex.intValue = 2 }
         GoodsSortChip(
             vm,
             GoodsSort.DESCENDING,
-            isSelected = selectedChipIndex.value == 3
-        ) { selectedChipIndex.value = 3 }
+            isSelected = selectedChipIndex.intValue == 3
+        ) { selectedChipIndex.intValue = 3 }
     }
 }
