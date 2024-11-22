e1ee542f6f36fb6a9bf50fe0583e7244b14360f2
thsamajiki <ant1102@naver.com>
2024-11-22 10:59:41 +0900

    Theme의 LightColorScheme 컬러 변경

--- app/src/main/java/com/hero/z_market/ui/theme/Theme.kt
+++ app/src/main/java/com/hero/z_market/ui/theme/Theme.kt
@@ -49,9 +49,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = primaryColor,
+    secondary = secondaryColor,
+    tertiary = tertiaryColor
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
