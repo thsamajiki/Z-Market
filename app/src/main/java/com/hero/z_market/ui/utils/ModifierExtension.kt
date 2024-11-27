821c897779fa0606ac16c8ba6e76fac59447b6cd
thsamajiki <ant1102@naver.com>
2024-11-27 11:51:00 +0900

    ModifierExtension 코드 개행 처리

--- app/src/main/java/com/hero/z_market/ui/utils/ModifierExtension.kt
+++ app/src/main/java/com/hero/z_market/ui/utils/ModifierExtension.kt
@@ -8,13 +8,15 @@ import androidx.compose.ui.layout.layout
 object ModifierExtension {
     fun Modifier.visible(isVisible: Boolean): Modifier = this.then(
         if (isVisible) Modifier
-        else Modifier.layout { _, constraints ->
+        else Modifier
+            .layout { _, constraints ->
             layout(0, 0) {} // 요소를 레이아웃 단계에서 완전히 숨김 -> Gone 상태로 만듦
         }
     )
 
     fun Modifier.gone(isGone: Boolean): Modifier = this.then(
-        if (isGone) Modifier.layout { _, constraints ->
+        if (isGone) Modifier
+            .layout { _, constraints ->
             layout(0, 0) {}
         }
         else Modifier
@@ -22,7 +24,8 @@ object ModifierExtension {
 
     fun Modifier.invisible(isInvisible: Boolean): Modifier = this.then(
         if (isInvisible) {
-            Modifier.alpha(0f) // 보이지 않게 처리
+            Modifier
+                .alpha(0f) // 보이지 않게 처리
                 .pointerInput(Unit) {} // 터치 등 상호작용 차단
         }
         else Modifier
